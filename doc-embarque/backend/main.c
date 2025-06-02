708b00044a599da383dc1367267a32f78bc40c8b
vicentelc06 <evnct3113gmail.com>
2025-06-01 21:04:02 -0300

    atualização

--- doc-embarque/backend/main.c
+++ doc-embarque/backend/main.c
@@ -4,26 +4,6 @@
 #include "include/services/user_service.h"
 
 int main() {
-    User novo;
-
-    novo.id = get_next_user_id();
-
-    printf("Nome: ");
-    fgets(novo.name, sizeof(novo.name), stdin);
-    strtok(novo.name, "\n");
-
-    printf("Email: ");
-    fgets(novo.email, sizeof(novo.email), stdin);
-    strtok(novo.email, "\n");
-
-    printf("Senha: ");
-    fgets(novo.password, sizeof(novo.password), stdin);
-    strtok(novo.password, "\n");
-
-    strcpy(novo.role, "ADMIN");
-
-    save_user(novo);
-    printf("Usuário cadastrado com sucesso!\n");
 
     return 0;
 }
