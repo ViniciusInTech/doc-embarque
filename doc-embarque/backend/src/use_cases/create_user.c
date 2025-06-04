39c1944bbd9a0913554df964efbd0410fe2969b7
viniciusintech <vini989073599@gmail.com>
2025-06-04 00:15:47 -0300

    feat: ajustando a arquitetura para caso de uso e somente isso

--- doc-embarque/backend/src/use_cases/create_user.c
+++ doc-embarque/backend/src/use_cases/create_user.c
@@ -1,17 +1,60 @@
 #include <stdio.h>
 #include <string.h>
+
+#ifdef _WIN32
+    #include <direct.h>
+#else
+    #include <sys/stat.h>
+    #include <sys/types.h>
+#endif
+
 #include "../../include/models/user.h"
-#include "../../include/services/user_service.h"
 #include "../../include/use_cases/create_user.h"
+
+#define DATA_DIR "../../data"
+#define FILE_PATH "../../data/users.txt"
+
+static void create_data_directory() {
+#ifdef _WIN32
+    _mkdir(DATA_DIR);
+#else
+    mkdir(DATA_DIR, 0777);
+#endif
+}
+
+static int get_next_user_id() {
+    FILE *file = fopen(FILE_PATH, "r");
+    int id = 0, temp;
+    User u;
+
+    if (file != NULL) {
+        while (fscanf(file, "%d;%[^;];%[^;];%[^;];%s\n", &temp, u.name, u.email, u.password, u.role) == 5) {
+            if (temp > id) id = temp;
+        }
+        fclose(file);
+    }
+
+    return id + 1;
+}
+
+static User save_user(User user) {
+    create_data_directory();
+
+    FILE *file = fopen(FILE_PATH, "a");
+    if (file == NULL) {
+        perror("Erro ao abrir o arquivo");
+        user.id = -1;
+        return user;
+    }
+
+    fprintf(file, "%d;%s;%s;%s;%s\n", user.id, user.name, user.email, user.password, user.role);
+    fclose(file);
+    return user;
+}
 
 User create_user(User user) {
     user.id = get_next_user_id();
-    User new_user = save_user(user);
-    if (new_user.id == -1) {
-        printf("Erro: o usuário não foi cadastrado.\n");
-        return new_user;
-    }
-    return new_user;
+    return save_user(user);
 }
 
 User create_user_cli() {
