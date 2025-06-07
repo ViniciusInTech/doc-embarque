dbedb938221e15b8d5cd576a7033445d9c432455
viniciusintech <vini989073599@gmail.com>
2025-06-06 21:44:50 -0300

    feat: criando endpoint de busca de usuario por id

---
+++ doc-embarque/backend/src/use_cases/get_user.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "../../include/models/user.h"
+#include "../../include/use_cases/get_user.h"
+
+#define FILE_PATH "../../data/users.txt"
+
+User get_user_by_id(int id) {
+    FILE *file = fopen(FILE_PATH, "r");
+    User user;
+    memset(&user, 0, sizeof(User));
+    user.id = -1;
+
+    if (file == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return user;
+    }
+
+    int temp;
+    User temp_user;
+    while (fscanf(file, "%d;%[^;];%[^;];%[^;];%s\n", &temp, temp_user.name, temp_user.email, temp_user.password, temp_user.role) == 5) {
+        if (temp == id) {
+            temp_user.id = temp;
+            user = temp_user;
+            break;
+        }
+    }
+
+    fclose(file);
+    return user;
+}
