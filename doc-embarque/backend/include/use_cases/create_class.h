6ccd1db3829fefd936cf6368fa966d691f500f1b
Giovanna Lima <giovannapslima.gpsl@gmail.com>
2025-06-09 01:58:16 -0300

    Update create_class.h

--- doc-embarque/backend/include/use_cases/create_class.h
+++ doc-embarque/backend/include/use_cases/create_class.h
@@ -4,14 +4,13 @@
 
 #ifndef CREATE_CLASS_H
 #define CREATE_CLASS_H
-<<<<<<< HEAD
+
 #include "../models/class.h"
 
 int class_exists(int id);
 void save_class(Class c);
 Class create_class_cli();
 
-=======
->>>>>>> origin/main
+
 
 #endif //CREATE_CLASS_H
